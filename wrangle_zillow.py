d4304805fb066d342b74467f9decfe8eab739013
Cody Clonts <clontscody@gmail.com>
2022-06-15 16:56:36 -0500

    updated wrangle file

--- wrangle_zillow.py
+++ wrangle_zillow.py
@@ -70,13 +70,62 @@ def split_zillow_data(df):
     return train, validate, test
 
 
+def handle_outliers(df, cols, k):
+    # Create placeholder dictionary for each columns bounds
+    bounds_dict = {}
+
+    # get a list of all columns that are not object type
+    non_object_cols = df.dtypes[df.dtypes != 'object'].index
+
+
+    for col in cols:
+        # get necessary iqr values
+        q1 = df[col].quantile(0.25)
+        q3 = df[col].quantile(0.75)
+        iqr = q3 - q1
+        upper_bound =  q3 + k * iqr
+        lower_bound =  q1 - k * iqr
+
+        #store values in a dictionary referencable by the column name
+        #and specific bound
+        bounds_dict[col] = {}
+        bounds_dict[col]['upper_bound'] = upper_bound
+        bounds_dict[col]['lower_bound'] = lower_bound
+
+    for col in cols:
+        #retrieve bounds
+        col_upper_bound = bounds_dict[col]['upper_bound']
+        col_lower_bound = bounds_dict[col]['lower_bound']
+
+        #remove rows with an outlier in that column
+        df = df[(df[col] < col_upper_bound) & (df[col] > col_lower_bound)]
+        return df
+
+def handle_outliers2(df):
+    df = df[(df.calculatedfinishedsquarefeet < 3900) & (df.calculatedfinishedsquarefeet > 500)]
+    df = df[(df.logerror < 3) & (df.logerror > -3)]
+    df = df[(df.bedroomcnt < 6) & (df.bedroomcnt > 1)]
+    df = df[( df.bathroomcnt < 4) & ( df. bathroomcnt > 1)]
+    df = df[(df.lotsizesquarefeet < 14000) & (df.lotsizesquarefeet > 700)]
+    df = df[(df.taxvaluedollarcnt < 3500000) & (df.taxvaluedollarcnt > 100000)]
+    return df
 
 
 
 def wrangle_zillow():
+    cols = [
+ 'bathroomcnt',
+ 'bedroomcnt',
+ 'calculatedfinishedsquarefeet',
+ 'lotsizesquarefeet',
+ 'taxvaluedollarcnt',
+ 'logerror']
     df =  get_zillow_data()
     df = handle_missing_values(df, prop_required_column = .9, prop_required_row = .9)
+    df = handle_outliers2(df)
+    df['month'] = pd.DatetimeIndex(df['transactiondate']).month
     return df
+
 
 
 
